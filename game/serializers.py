ef207d35424f7bc33257c0b814fbdcf7267819eb
Adah Eni <eniwokepdm@gmail.com>
2020-07-24 07:14:17 +0100

    refactor: add Serializers for Newsletter and Contactus

--- game/serializers.py
+++ game/serializers.py
@@ -2,43 +2,43 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-from .models import Game, Question, Options, UserGames, Category
+from .models import Game, Question, Options, UserGames, Category, Newsletter, ContactUs
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = '__all__'
+        fields = "__all__"
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = '__all__'
+        fields = "__all__"
 
 
 class OptionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Options
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserGamesSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserGames
-        fields = '__all__'
+        fields = "__all__"
 
 
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'email', 'password', 'username']
+        fields = ["id", "email", "password", "username"]
 
 
 class ResetUserPasswordSerializer(serializers.ModelSerializer):
@@ -47,10 +47,22 @@ class ResetUserPasswordSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ['current_password', 'password']
+        fields = ["current_password", "password"]
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = "__all__"
+
+
+class NewsletterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Newsletter
+        fields = "__all__"
+
+
+class ContactUsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactUs
+        fields = "__all__"
