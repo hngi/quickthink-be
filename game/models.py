7a3fe978cde075ee5af4aa623a4ca8050a49a0a1
Adah Eni <eniwokepdm@gmail.com>
2020-07-24 07:16:30 +0100

    refactor: add import for dattime

--- game/models.py
+++ game/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
-
+from datetime import datetime
 
 # Create your models here.
 def add_one():
